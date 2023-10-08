3ad83acb533d7262ac917c45098cfd8f927baf23
email <racbernazgul@gmail.com>
2023-10-08 22:31:01 +0300

    prv

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-
 const PrivateRouter = () => {
-  const user = true
-  return user ? <Outlet/> : <Navigate to="/login"/>
+  const user = true;
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRouter;
