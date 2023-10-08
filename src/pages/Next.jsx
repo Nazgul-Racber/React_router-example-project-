9271b3780d9ef8b077f77f6bed5fb5ce1062abbe
email <racbernazgul@gmail.com>
2023-10-08 15:56:38 +0300

    react/next

---
+++ src/pages/Next.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Next = () => {
+  return (
+    <div>
+      <h1>Next.jsx</h1>
+    </div>
+  )
+}
+
+export default Next
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Nav from "../components/Nav";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
@@ -10,6 +10,8 @@ import NotFound from "../pages/NotFound";
 import Footer from "../components/Footer";
 import Fullstack from "../pages/Fullstack";
 import Aws from "../pages/Aws";
+import Next from "../pages/Next";
+import React from "../pages/React";
 
 const AppRouter = () => {
   return (
@@ -18,9 +20,12 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/paths" element={<Paths />}>
-          <Route index element={<Fullstack/>} />
-          <Route path="fullstack" element={<Fullstack/>} />
-          <Route path="aws" element={<Aws/>} />
+          <Route index element={<Fullstack />} />
+          <Route path="fullstack" element={<Fullstack />}>
+            <Route Path="react" element={<React/>} />
+            <Route Path="Next" element={<Next/>} />
+          </Route>
+          <Route path="aws" element={<Aws />} />
         </Route>
         <Route path="/people" element={<People />} />
         <Route path="/people/:id" element={<PersonDetail />} />
