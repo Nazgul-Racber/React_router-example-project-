af174865000061444381c8594ac961db086cb7ef
email <racbernazgul@gmail.com>
2023-10-08 23:07:39 +0300

    login:set

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+  return (
+    <div className="container mt-4">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
